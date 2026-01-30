f8f14d42c43e935124e41bafc89c6c7eadaf1a2c
julio gabriel <juliogabriel78956@gmail.com>
2026-01-30 16:10:32 -0300

    DO while

---
+++ src/DoWhile/Contando.java
@@ -0,0 +1,16 @@
+package DoWhile;
+
+public class Contando {
+    public static void main(String[] args) {
+        int i = 9;
+        int max = 20;
+        System.out.println("Contando até " + max);
+
+        while (i < max) {
+            System.out.println("Valor de i: " + i);
+            i++;
+        }
+        System.out.println(i);
+
+    }
+}
---
+++ src/For/LoopFor.java
@@ -0,0 +1,10 @@
+package For;
+
+public class LoopFor {
+    public static void main(String[] args) {
+
+        for (int i = 1;  i <20  ; i++) {
+            System.out.println(i);
+        }
+    }
+}
