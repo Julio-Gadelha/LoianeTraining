1e6fd4dffe9876c4fc418d66acfb184311f3562b
julio gabriel <juliogabriel78956@gmail.com>
2026-02-24 12:37:57 -0300

    Exercise

---
+++ src/Exercicios2/Carteira.java
@@ -0,0 +1,10 @@
+package Exercicios2;
+
+import java.util.Scanner;
+
+public class Carteira {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner (System.in);
+     
+    }
+}
