7894825462b588d8b6beeddcccc507d9ef627e1f
julio gabriel <juliogabriel78956@gmail.com>
2026-03-24 11:14:13 -0300

    Exericicios

---
+++ src/Exercicios/Graus.java
@@ -0,0 +1,15 @@
+package Exercicios;
+
+import java.util.Scanner;
+
+public class Graus {
+    static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Digite os graus em Farenhait : ");
+        int faren = scan.nextInt();
+        scan.close();
+
+         int celsius = (  5 * faren) /9;
+        System.out.println(" O resultado em graus e :  " + celsius);
+    }
+}
