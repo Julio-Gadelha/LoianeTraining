1bc3c2b9732ea8905b8b2be32f0fdc81a0f4e345
julio gabriel <juliogabriel78956@gmail.com>
2026-04-03 10:21:11 -0300

    Exercise3

---
+++ src/Exercicse3/Tabuada.java
@@ -0,0 +1,16 @@
+package Exercicse3;
+
+import java.util.Scanner;
+
+public class Tabuada {
+  public   static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+      System.out.println("Digite um numero inteiro : ");
+      int num = sc.nextInt();
+
+      for (int i = 1; i <11 ; i++) {
+          System.out.println(num + " x " + i + " = " + (num* i));
+      }
+    }
+}
